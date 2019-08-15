92e55ea91bd1fcf402d8b7ebed10a10ea5c2236d
Renato Filho <renatosousafilho@gmail.com>
2019-08-15 00:31:57 -0300

    itsamatch

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import io from 'socket.io-client'
 import './Main.css'
 
 import api from '../services/api'
@@ -6,9 +7,11 @@ import api from '../services/api'
 import logo from '../assets/logo.svg'
 import like from '../assets/like.svg'
 import dislike from '../assets/dislike.svg'
+import itsamatch from '../assets/itsamatch.png'
 
 export default function Main({ match }) {
     const [users, setUsers] = useState([]);
+    const [matchDev, setMatchDev] = useState(false)
     
     useEffect(() => {
         async function loadUsers() {
@@ -23,6 +26,16 @@ export default function Main({ match }) {
 
         loadUsers()
     }, [match.params.id]);
+
+    useEffect(() => {
+        const socket = io('http://localhost:3330', {
+            query: { user: match.params.id }
+        })
+
+        socket.on('match', dev => {
+            setMatchDev(dev)
+        })
+    }, [match.params.id])
 
     async function handleLike(id) {
         await api.post(`/devs/${id}/likes`, {}, {
@@ -71,6 +84,17 @@ export default function Main({ match }) {
                 </ul>
             ) : (
                 <div className='empty'>Acabou :(</div>
+            )}
+
+            { matchDev && (
+                <div className='match-container'>
+                    <img src={itsamatch} alt="" />
+                    <img className="avatar" src={matchDev.avatar} />
+                    <strong>{matchDev.name}</strong>
+                    <p>{matchDev.bio}</p>
+
+                    <button type='button' onClick={() => setMatchDev(null)}>Fechar</button>
+                </div>
             )}
         </div>
     )
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 const axios = require('axios')
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: 'http://localhost:3330'
 });
 
 export default api;
